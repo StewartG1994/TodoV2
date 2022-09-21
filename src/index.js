7a3a0c7084c0215c03a88e451c9eb04b3bef2145
Stewart Gliniecki <stewgliniecki@gmail.com>
2022-09-21 21:29:46 +0100

    Function additions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './style.css'
-import { projectArray } from './info'
+import { projectArray, todoArrayAddition,todoFactory } from './info'
+todoArrayAddition(projectArray[0].todoArray,todoFactory('test','test','test','test','test'))
+todoArrayAddition(projectArray[0].todoArray,todoFactory('test2','test2','test2','test2','test2'))
+
 console.log(projectArray)
--- src/info.js
+++ src/info.js
@@ -1,21 +1,24 @@
 const projectArray = [];
-const projectFactory = (title, description, dueDate, priority, notes, checklist) =>{
-    return {title, description, dueDate, priority ,notes , checklist}
+const projectFactory = (title, description, dueDate, priority, notes, checklist, todoArray) =>{
+    todoArray = []
+
+    return {title, description, dueDate, priority ,notes , checklist, todoArray}
 }
 
-const todoFactory = (task, description,dueDate, priority, completed) =>{
-     return task, description,dueDate, priority, completed}
+const todoFactory = (task, description,dueDate, priority, completed) =>
+ {  return{ task, description,dueDate, priority, completed}}
 
 
 function projectAddition (item) {
-    projectArray.push(item); 
-   
+    projectArray.push(item);    
 }
 
-
+function todoArrayAddition (array, item) {
+    array.push(item)
+}
 
 
 projectAddition(projectFactory('Create new logo', 'Create a new logo design to be used throughout new business launch', '26/02/2023', 'High', 'To be updated', 'test'))
 
 
-export {projectArray, }
+export {projectArray,todoArrayAddition, todoFactory }
