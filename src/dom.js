3c6cde844f90ae5ecb289ed1dc3757a42d80d7cf
Stewart Gliniecki <stewgliniecki@gmail.com>
2022-09-23 17:30:21 +0100

    Project Dom functions

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,5 @@
 const projectListNode = document.querySelector('.projectList');
+const content = document.querySelector('.content')
 
 function projectListDom(array){
     for (let i = 0; i < array.length; i++) {
@@ -10,10 +11,50 @@ function projectListDom(array){
     }
 }
 
+function cardMaker (item) {
+    const card = document.createElement('div');
+    card.className = 'projectCards'
+    const header = document.createElement('h1');
+    const description = document.createElement('p');
+    const dueDate = document.createElement('p');
+    const priority = document.createElement('p');
+    const notes = document.createElement('p');
+
+    header.textContent = item.title
+    description.textContent = item.description;
+    dueDate.textContent = item.dueDate;
+    notes.textContent = item.notes;
+
+    const checkboxdiv = document.createElement('div');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'checkboxId';
+    checkbox.name = 'checklist';
+    checkbox.value = 'checklist';
+    const checkboxLabel = document.createElement('label')
+    checkboxLabel.htmlFor = 'checkbox'
+    checkboxLabel.textContent = item.checklist;
+    
+    card.appendChild(header);
+    card.appendChild(description);
+    card.appendChild(dueDate);
+    card.appendChild(priority);
+    card.appendChild(notes);
+    card.appendChild(checkboxLabel)
+    checkboxdiv.appendChild(checkbox)
+    checkboxdiv.appendChild(checkboxLabel);
+    card.appendChild(checkboxdiv)
+    content.appendChild(card)
+}
+
+
 function projectCardDisplay(array){
     for (let i = 0; i < array.length; i++){
+        cardMaker(array[i])
 
     }
 }
 
-export {projectListDom}
+export {projectListDom , projectCardDisplay}
+
+//title, description, dueDate, priority, notes, checklist, todoArray
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './style.css'
 import { projectArray} from './info'
-import {projectListDom} from './dom'
+import {projectListDom, projectCardDisplay} from './dom'
 console.log(projectArray)
 projectListDom(projectArray)
+projectCardDisplay(projectArray)
 const projectListNode = document.querySelector('.projectList');
+
 
 console.log(projectListNode)
